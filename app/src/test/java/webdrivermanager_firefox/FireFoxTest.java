b87af9dc6a67f2a69a719ebdb51f3f37f50dbdf6
kazurayam <kazuaki.urayama@gmail.com>
2024-05-04 09:54:08 +0900

    first commit

---
+++ app/src/test/java/webdrivermanager_firefox/FireFoxTest.java
@@ -0,0 +1,42 @@
+package webdrivermanager_firefox;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class FireFoxTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
+
+        // Verify
+        assertThat(title).contains("Selenium WebDriver");
+    }
+}
